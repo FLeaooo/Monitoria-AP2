1f6bdb656a02ac30a2ecb49c7e8a520e8c6ed88b
FLeaooo <fernandoleaosouzasaliba@gmail.com>
2024-09-08 20:54:23 -0300

    Feat: started lab3

---
+++ listas/Lab_03/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int numero_global = 2;
+#include "q1.h"
+
+int main() {
+  int resp = impar_par();
+  printf("%d\n", resp);
+  return 0;
+}
---
+++ listas/Lab_03/q1.h
@@ -0,0 +1,7 @@
+// minha_biblioteca.h
+#ifndef Q1_H
+#define Q1_H
+
+int impar_par() { return numero_global % 2 != 0; }
+
+#endif
